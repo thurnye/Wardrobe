5e2da7986a2e8db5e5f2a1bcd2c5833cb57943fd
Daniel Tamunotonye <dan_tonye@Gudi.local>
2023-03-16 12:48:52 -0400

    mirrorlayout

--- src/pages/Wardrobe/Wardrobe.tsx
+++ src/pages/Wardrobe/Wardrobe.tsx
@@ -1,4 +1,4 @@
-import { Card, Typography } from '@mui/material';
+import { Box, Card, CardContent, Typography } from '@mui/material';
 import React, { FC } from 'react';
 import styles from './Wardrobe.module.scss';
 
@@ -6,11 +6,43 @@ interface WardrobeProps {}
 
 const Wardrobe: FC<WardrobeProps> = () => (
   <div className={styles.Wardrobe} data-testid="Wardrobe">
-    <Card sx={{height: 'inherit'}}>
-      <Typography sx={{ fontSize: 13, p:1, m:0 }} color="text.secondary" gutterBottom>
-        FOOTER
-      </Typography>
-    </Card>
+    <Box className={styles.wardrobeContainer}>
+        
+        {/* Closet */}
+        <Box className={styles.closetContainer}>
+          <Card sx={{height: 'inherit'}}>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                CLOSET
+              </Typography>
+            </CardContent>
+          </Card>
+        </Box>
+
+        {/* Infographic */}
+        <Box className={styles.infographicContainer}>
+          <Card sx={{height: 'inherit', display: 'flex',flexDirection: 'column', p:2 }}>
+            {/* <CardContent> */}
+              
+                <Box className={styles.displayMirror}>
+                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                     Mirror
+                    </Typography>
+                </Box>
+                <Box className={styles.positions}>
+                  <span className={styles.positionSpan}></span>
+                  <span className={styles.positions}></span>
+                  <span className={styles.positions}></span>
+                </Box>
+                <Box className={styles.displayInfo}>
+                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                     item info
+                    </Typography>
+                </Box>
+            {/* </CardContent> */}
+          </Card>
+        </Box>
+      </Box>
   </div>
 );
 
